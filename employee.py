8e6bfa548a915e9aaaf40fb846773437d73aae56
Dmytro Oliinyk <oliynykdmitry83@gmail.com>
2023-11-15 05:12:46 +0000

    first initialisation of a class

--- employee.py
+++ employee.py
@@ -1,3 +1,21 @@
 # Let's start!
 
 #  OOP introduction
+
+class Employee:
+    name = "No Name"
+    dept =  "No Department"
+
+def getName(self):
+    return self.name
+
+def getDept(self):
+    return self.dept
+
+def setName(self, xname):
+    self.name = xname
+    return
+
+def setDept(self, xdept):
+    self.dept = xdept
+    return
